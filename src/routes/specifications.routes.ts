ff35246b5040cc1224c539ed8552f2ba3afdd259
Vinicius Benedito <beneditoloura@gmail.com>
2022-07-19 10:57:54 -0300

    ✨ feat: create Specifications methods

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRoutes = Router();
+
+const specificationsRepository = new SpecificationRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationRoutes };
