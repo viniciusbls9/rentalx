13f71a5a9d6338a82121acd384cd52fd41fe8991
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-03 07:12:49 -0300

    ✨ feat? optimize routers import

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,22 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationsRepository = new SpecificationRepository();
-
 specificationRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };
